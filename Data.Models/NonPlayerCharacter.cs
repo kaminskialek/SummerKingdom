ac77f28a7d727fc76b8215c1039ca328821c27d5
alexkalionski <alexkalionski@gmail.com>
2024-06-25 18:47:42 +0300

    Removed unnecessary usings.

--- Data.Models/NonPlayerCharacter.cs
+++ Data.Models/NonPlayerCharacter.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data.Models
 {
     public class NonPlayerCharacter : IsDeletable
     {
         [Required, Key]
-        public int Id {  get; set; }
+        public int Id { get; set; }
 
         [Required, ForeignKey("Character")]
         public int CharacterId { get; set; }
