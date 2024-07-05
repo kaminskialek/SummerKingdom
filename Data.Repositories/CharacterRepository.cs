c33a4ad3b8041050ac7154e411b5e9c14f2de8f7
alexkalionski <alexkalionski@gmail.com>
2024-07-05 17:38:59 +0300

    Added Character Repository contract (to be implemented).

---
+++ Data.Repositories/CharacterRepository.cs
@@ -0,0 +1,53 @@
+﻿using Data.Models;
+using Data.Repositories.Contracts;
+
+namespace Data.Repositories
+{
+    public class CharacterRepository : ICharacterRepository
+    {
+        public NonPlayerCharacter CreateNonPlayerCharacter(NonPlayerCharacter nonPlayerCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerCharacter CreatePlayerCharacter(PlayerCharacter playerCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<NonPlayerCharacter> GetAllNonPlayerCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlayerCharacter> GetAllPlayerCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NonPlayerCharacter GetNonPlayerCharacter(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerCharacter GetPlayerCharacter(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NonPlayerCharacter UpdateNonPlayerCharacter(NonPlayerCharacter nonPlayerCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerCharacter UpdatePlayerCharacter(PlayerCharacter playerCharacter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data.Repositories/Contracts/ICharacterRepository.cs
@@ -0,0 +1,34 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories.Contracts
+{
+    public interface ICharacterRepository
+    {
+        public NonPlayerCharacter GetNonPlayerCharacter(int id);
+
+        public PlayerCharacter GetPlayerCharacter(int id);
+
+        public List<NonPlayerCharacter> GetAllNonPlayerCharacters();
+
+        public List<PlayerCharacter> GetAllPlayerCharacters();
+
+        public NonPlayerCharacter CreateNonPlayerCharacter(NonPlayerCharacter nonPlayerCharacter);
+
+        public NonPlayerCharacter UpdateNonPlayerCharacter(NonPlayerCharacter nonPlayerCharacter);
+
+        public PlayerCharacter CreatePlayerCharacter (PlayerCharacter playerCharacter);
+
+        public PlayerCharacter UpdatePlayerCharacter(PlayerCharacter playerCharacter);
+
+        public Character Delete(int id);
+
+
+
+       
+    }
+}
