a5464078965864b76f3d88c75d76aa1ccf99aedf
alexkalionski <alexkalionski@gmail.com>
2024-07-05 17:31:12 +0300

    Added User Repository contract (to be implemented).

---
+++ Data.Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Data.Models;
+
+namespace Data.Repositories.Contracts
+{
+    public interface IUserRepository
+    {
+        public User GetById (int id);
+        
+        public User GetByUserName (string userName);
+
+        public User GetByEmail (string email);
+
+        public List<User> GetAll();
+
+        public User Create(User user);
+
+        public User Update(int id, User userToUpdate);
+
+        public User Delete(int id);
+    }
+}
---
+++ Data.Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Data.Models;
+using Data.Repositories.Contracts;
+
+namespace Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public User Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByUserName(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(int id, User userToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
