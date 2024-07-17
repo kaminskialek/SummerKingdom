277bf8228508d8f6e53fbab0c11ae0b3f8d65f22
alexkalionski <alexkalionski@gmail.com>
2024-07-17 13:31:52 +0300

    Added CRUD operations to Faction Repository.

--- Data.Repositories/FactionRepository.cs
+++ Data.Repositories/FactionRepository.cs
@@ -1,23 +1,36 @@
-﻿using Data.Models;
+﻿using Common.Exceptions;
+using Data.Models;
 using Data.Repositories.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data.Repositories
 {
     public class FactionRepository : IFactionRepository
     {
+        private readonly ApplicationDbContext factionContext;
+
+        private const string factionNotFoundErrorMessage = "Faction with id {0} cannot be found.";
+        public FactionRepository(ApplicationDbContext context)
+        {
+            this.factionContext = context;
+        }
         public Faction Create(Faction faction)
         {
-            throw new NotImplementedException();
+            factionContext.Factions.Add(faction);
+
+            factionContext.SaveChanges();
+
+            return faction;
         }
 
         public Faction Delete(int id)
         {
-            throw new NotImplementedException();
+            Faction factionToDelete = GetById(id);
+
+            factionToDelete.DeletedOn = DateTime.Now;
+
+            factionContext.SaveChanges();
+
+            return factionToDelete;
         }
 
         public List<Faction> GetAll()
@@ -27,12 +40,24 @@ namespace Data.Repositories
 
         public Faction GetById(int id)
         {
-            throw new NotImplementedException();
+            Faction faction = factionContext.Factions.FirstOrDefault(f => f.Id == id)
+             ?? throw new EntityNotFoundException(string.Format(factionNotFoundErrorMessage, id));
+
+            return faction;
         }
 
         public Faction Update(Faction faction, int id)
         {
-            throw new NotImplementedException();
+            Faction updatedFaction = GetById(id);
+
+            updatedFaction.Name = faction.Name;
+            updatedFaction.Description = faction.Description;   
+            updatedFaction.Allies = faction.Allies;
+            updatedFaction.Opponents = faction.Opponents;
+
+            factionContext.SaveChanges();
+
+            return updatedFaction;
         }
     }
 }
