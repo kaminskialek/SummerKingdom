b9131e1f6173720bdea07b009860d1b5dce2b4ef
alexkalionski <alexkalionski@gmail.com>
2024-07-17 17:51:28 +0300

    Added Lore Entry CRUD operations.

--- Data.Repositories/LoreEntryRepository.cs
+++ Data.Repositories/LoreEntryRepository.cs
@@ -1,18 +1,37 @@
-﻿using Data.Models;
+﻿using Common.Exceptions;
+using Data.Models;
 using Data.Repositories.Contracts;
 
 namespace Data.Repositories
 {
     public class LoreEntryRepository : ILoreEntryRepository
     {
+        private readonly ApplicationDbContext loreEntryContext;
+
+        private const string loreEntryNotFoundErrorMessage = "Lore entry with id {0} cannot be found.";
+
+        public LoreEntryRepository(ApplicationDbContext context) 
+        {
+            this.loreEntryContext = context;
+        }
         public LoreEntry Create(LoreEntry loreEntry)
         {
-            throw new NotImplementedException();
+            loreEntryContext.LoreEntries.Add(loreEntry);
+
+            loreEntryContext.SaveChanges();
+
+            return loreEntry;
         }
 
         public LoreEntry Delete(int id)
         {
-            throw new NotImplementedException();
+            LoreEntry loreEntryToDelete = GetById(id);
+
+            loreEntryToDelete.DeletedOn = DateTime.Now;
+
+            loreEntryContext.SaveChanges();
+
+            return loreEntryToDelete;
         }
 
         public List<LoreEntry> GetAll()
@@ -22,12 +41,23 @@ namespace Data.Repositories
 
         public LoreEntry GetById(int id)
         {
-            throw new NotImplementedException();
+            LoreEntry loreEntry = loreEntryContext.LoreEntries
+            .FirstOrDefault(l => l.Id == id) ??
+            throw new EntityNotFoundException(string.Format(loreEntryNotFoundErrorMessage, id));
+
+            return loreEntry;
         }
 
         public LoreEntry Update(LoreEntry loreEntry, int id)
         {
-            throw new NotImplementedException();
+            LoreEntry updatedLoreEntry = GetById(id);
+
+            updatedLoreEntry.Title = loreEntry.Title;
+            updatedLoreEntry.Content = loreEntry.Content;
+
+            loreEntryContext.SaveChanges();
+
+            return updatedLoreEntry;
         }
     }
 }
